a69aae525674e9e8e01cd933c3431b90f85c07c2
Kim Young Wan <powerkyw59@naver.com>
2020-07-03 08:57:51 +0900

    마무리
    버그 픽스 완료, MagicBox의 통과가 제대로 이루어지지않는 점을 해결하였음.
    동일한 코루틴이 동시에 돌고 있어서 문제였고, 하나만 돌도록 하여서 해결함

--- termProject/Assets/Scripts/Objects/MagicBlock.cs
+++ termProject/Assets/Scripts/Objects/MagicBlock.cs
@@ -8,24 +8,24 @@ public class MagicBlock : MonoBehaviour
     public float coolTime;
     public float limitedTime;
     SpriteRenderer spriteRenderer;
-    private float timeValue;
-    private float limitedTimeValue;
+    public float timeValue;
+    public float limitedTimeValue;
     void Start()
     {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         timeValue = 0;
         limitedTimeValue = 0;
-        StartCoroutine("callCorutine");
+        StartCoroutine(callCorutine());
     }
     void Update()
     {
-        callCorutine().MoveNext();
+        //callCorutine().MoveNext();
     }
     // Update is called once per frame
     IEnumerator callCorutine()
     {
         PlatformEffector2D collider = gameObject.GetComponent<PlatformEffector2D>();
-
+        Rigidbody2D rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
         while (true)
         {
             while (limitedTimeValue <= limitedTime)
@@ -33,7 +33,7 @@ public class MagicBlock : MonoBehaviour
                 limitedTimeValue += Time.deltaTime;
                 yield return null;
             }
-            limitedTimeValue = 0;
+            limitedTimeValue = 0.0f;
 
             while (spriteRenderer.color.a > 0.0f)
             {
@@ -43,15 +43,17 @@ public class MagicBlock : MonoBehaviour
             }
             //각도 0
             collider.surfaceArc = 0;
+            rigidbody2D.simulated = false;
             while (timeValue <= coolTime)
             {
                 timeValue += Time.deltaTime;
                 yield return null;
             }
 
-            timeValue = 0;
+            timeValue = 0.0f;
 
             collider.surfaceArc = 180;
+            rigidbody2D.simulated = true;
             while (spriteRenderer.color.a < 1.0f)
             {
                 spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g,
